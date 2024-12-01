efd75fbe6c14e9c866d07e30b7672ce0cd265b67
Lienhop <hans.lienhop@mail.com>
2024-12-01 15:37:06 +0100

    Finished part 2

--- 1/main.py
+++ 1/main.py
@@ -2,6 +2,7 @@
 def main():
 
     a, b = [], []
+    dict  = {}
 
     with open('data.csv') as f:
         lines = f.readlines()
@@ -13,8 +14,17 @@ def main():
     b.sort()
     sum = 0
     for i, j in zip(a, b):
+        if j not in dict:
+            dict[j] = 0
+        dict[j] += 1
         sum += abs(i -j)
     print(sum)
-
+    sum2 = 0
+    for i in a:
+        if i not in dict:
+            continue
+        sum2 +=  i * dict[i]
+    print(sum2)
+    
 if __name__ == '__main__':
     main()
